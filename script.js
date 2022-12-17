ee0a697df604d8eed780cb7a0ba79325acc99498
icika-max <icika-max>
2022-12-17 08:38:59 +0300

    adding details of the restaurant

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ document.addEventListener("DOMContentLoaded", () => {
             <h2>${restaurant.name}</h2>
             </div>
             <img src=${restaurant.image} alt="#"/>
+            
+            <div class="card-restaurant-details">
+            <div class="country">
+                <p><span><<i class="fa fa-flag" aria-hidden="true"></i></span>county: ${restaurant.county}</p>
+
+            </div>
 
     })
 
