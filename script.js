95e6752af9075f355213cf6b4b90c88404fd93dc
icika-max <icika-max>
2022-12-17 06:48:16 +0300

    fetch data from json

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-fetch('http://localhost:3000/characters')
-.then(data=>{})
+    const container = document.querySelector(".container")
+    const fetchData=()=>
+    {    fetch('http://localhost:3000/characters')
+        .then(res=>res.json())
+        .then((data)=>{
+            console.log(data);
 
+        })
 
 
 
