33abd417a8fffa59116fbf76d929cb326c1baf4a
icika-max <icika-max>
2022-12-17 08:26:58 +0300

    fetch data from json

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container")
-    const fetchData=()=>
-    {    fetch('http://localhost:3000/characters')
-        .then(res=>res.json())
-        .then((data)=>{
-            console.log(data);
+    const searchInput=document.querySelector("[data-search]")
+    searchInput.addEventListener("input",e =>{
+    const value = e.target.value
+    restaurant.forEach(user=>{
+        const isVisible=user.name.includes(value)
+        user.classLst.toggle("hide",!isVisible)
+    })
 
-        })
-
-
-
-
-
-
-
-
+    })
 })
